f717dd48fe116441e060a0eb81163206009ac01b
Robert Dąbrowski <r.dabrowski@uw.edu.pl>
2016-05-18 13:11:14 +0200

    Stabilizing towards first release

---
+++ src/draw.js
@@ -0,0 +1,83 @@
+function drawThePicture(error, graph) {
+
+  if (error) throw error;
+
+  d3.select(window).on("resize", onResize);
+
+  force = d3.layout.force()
+      .size([width, height])
+      .charge(-120)
+      .linkDistance(20)
+      .on("tick", onTick);
+
+  var color = d3.scale.category20();
+  var drag = force.drag().on("dragstart", dragstart);
+
+  /*
+  var drag = d3.behavior.drag()
+      .origin(function(d) { return d; })
+      .on("dragstart", dragstarted)
+      .on("drag", dragged)
+      .on("dragend", dragended);
+  */
+
+  link = svg.selectAll(".link")
+      .data(graph.links)
+      .enter()
+      .append("line")
+      .attr("class", "link")
+      .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+  node = svg.selectAll(".node")
+      .data(graph.nodes)
+      .enter()
+      .append("circle")
+      .attr("class", "node")
+      .attr("r", 5)
+      .style("fill", function(d) { return color(d.group); })
+      //.text(function(d) { return d.name + "\n" + d.url; })
+      .on("mouseover", handleMouseOver)
+      .on("mouseout", handleMouseOut)
+      .on("contextmenu", rgtclick)
+      //.on("dblclick", dblclick)
+      .on("click", function(d) {
+        if (d3.event.defaultPrevented) return; // click suppressed
+        d3.select().classed("fixed", d.fixed = false);
+      })
+      .call(
+        force.drag().on("dragstart", dragstart)
+      );
+
+  // Tooltips handled by the browser
+  svg.selectAll("circle")
+     .append("title")
+     .text(function(d) { return d.name + "\n" + d.url; });
+
+  force
+      .nodes(graph.nodes)
+      .links(graph.links);
+
+  force.start();
+
+} // draw the picture
+
+function updateThePicture(error, graph) {
+
+  if (error) throw error;
+
+  force.resume();
+
+  var n = force.nodes();
+  var l = force.links();
+
+  for (i = 0; i <= 94; i++) {
+      n[i].x = graph.nodes[i].x; //n[i].x + 100;
+      n[i].y = graph.nodes[i].y; //n[i].y + 100;
+      n[i].px = n[i].x;
+      n[i].py = n[i].y;
+      n[i].fixed = true;
+  };
+
+  svg.selectAll('.node').classed("fixed", function(d) {d.fixed = true} );
+
+} // update the picture
---
+++ src/events.js
@@ -0,0 +1,136 @@
+function clickGithub() {
+  shell.openExternal("http://github.com");
+}
+
+function clickSova() {
+  shell.openExternal("http://www.uw.edu.pl");
+}
+
+function clickMagnify() {
+  shell.openExternal("http://www.mimuw.edu.pl");
+}
+
+function clickZoomIn() {
+  console.log("Zoom in");
+}
+
+function clickZoomOut() {
+  console.log("Zoom out");
+}
+
+function clickZoomFit() {
+  console.log("Zoom fit");
+}
+
+function clickFiles() {
+  shell.openExternal("http://www.mimuw.edu.pl");
+}
+
+function clickClasses() {
+  shell.openExternal("http://www.mimuw.edu.pl");
+}
+
+function clickCommits() {
+  shell.openExternal("http://www.mimuw.edu.pl");
+}
+
+function clickAuthors() {
+  shell.openExternal("http://www.mimuw.edu.pl");
+}
+
+function clickFreeze() {
+  d3.layout.force().stop();
+  d3.select('body').select('svg').selectAll('.node').classed("fixed", function(d) {d.fixed = true} );
+}
+
+function clickUnfreeze() {
+  d3.layout.force().resume();
+  d3.select('body').select('svg').selectAll('.node').classed("fixed", function(d) {d.fixed = false} );
+}
+
+function clickReload() {
+  d3.layout.force().stop();
+  d3.layout.force().start();
+  d3.select('body').select('svg').selectAll('.node').classed("fixed", function(d) {d.fixed = false} );
+  d3.layout.force().resume();
+}
+
+function rgtclick(d, i) {
+  //clipboard.writeText(d.url);
+  //window.open(d.url)
+  shell.openExternal(d.url);
+}
+
+function dblclick(d) {
+  //d3.select(this)
+  d3.select().classed("fixed", d.fixed = false);
+  //d3.event.sourceEvent.stopPropagation();
+}
+
+function dragstart(d) {
+  //d3.select(this)
+  d3.select().classed("fixed", d.fixed = true);
+  d3.event.sourceEvent.stopPropagation();
+}
+
+function onResize() {
+  width = window.innerWidth, height = window.innerHeight;
+  d3.select('body').select('svg').attr("width", width).attr("height", height);
+  force.size([width, height]).resume();
+}
+
+function onTick() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+}
+
+function handleMouseOver(d, i) {
+  // Use D3 to select element, change color and size
+  d3.select(this).attr({
+    //fill: "orange",
+    r: 10
+  });
+  //div.transition()
+  //   .duration(500)
+  //   .style("opacity", 0);
+  //div.transition()
+  //   .duration(200)
+  //   .style("opacity", .9);
+  //div.html(
+  //      '<a href="http://google.com">'+ // The first <a> tag
+  //      //formatTime(d.date)+
+  //      'google.com' +
+  //      "</a>" +                          // closing </a> tag
+  //      "<br/>"  +
+  //      //d.close
+  //      'ala ma kota'
+  //    )
+  //    .style("left", (d3.event.pageX) + "px")
+  //    .style("top", (d3.event.pageY - 28) + "px");
+  // Specify where to put label of text
+  //svg.append("text").attr({
+  //   id: "t" + d.x + "-" + d.y + "-" + i,  // Create an id for text so we can select it later for removing on mouseout
+  //    x: function() { return xScale(d.x) - 30; },
+  //    y: function() { return yScale(d.y) - 15; }
+  //})
+  //.text(function() {
+  //  return [d.x, d.y];  // Value of the text
+  //});
+}
+
+function handleMouseOut(d, i) {
+  // Use D3 to select element, change color back to normal
+  d3.select(this).attr({
+    //fill: "black",
+    r: 5
+  });
+  //div.transition()
+  //   .duration(500)
+  //   .style("opacity", 0);
+  // Select text by id and then remove
+  //d3.select("#t" + d.x + "-" + d.y + "-" + i).remove();  // Remove text location
+}
